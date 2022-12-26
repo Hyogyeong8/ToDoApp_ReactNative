04ff47db58b56eb8695da58d9f18221a4a8422a1
Hyogyeong8 <khg4773@gmail.com>
2022-12-26 15:45:39 +0900

    Add(props): MyButton.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ export default function App() {
       <Text>Hello, Diana</Text>
       <StatusBar style="auto" />
       <Button title="Login" onPress={() => alert("Step to login")} />
+      <MyButton title="MyButton 1" onPress={() => alert("1")} />
+      <MyButton title="MyButton 2" onPress={() => alert("2")}>
+        MyButton3
+      </MyButton>
+      <MyButton>MyChildren</MyButton>
       <MyButton />
+      {/* <MyButton title={12} onPress={() => alert("3")} /> */}
+      {/* 위의 숫자 12 버튼은 title의 type이 string이 아니면 
+      warning을 띄운다는 것을 보여주기 위함이다 */}
     </View>
   );
 }
--- src/MyButton.js
+++ src/MyButton.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { TouchableOpacity, View, Text } from "react-native";
+import PropTypes from "prop-types";
 
-const MyButton = () => {
+const MyButton = ({ title, onPress, children, subtitle }) => {
+  // const MyButton = ({ title = "title", onPress = () => {}, children }) => {
+  // 위와 같이 props의 기본값을 설정할 수 있다(1번 방법)
+  // console.log(props);
   return (
     <TouchableOpacity
-      onPress={() => alert("MyButton works!")}
+      onPress={onPress}
       // hitSlop={{ bottom: 10 }}
       // pressRetentionOffset={{ bottom: 10 }}
     >
@@ -14,10 +18,26 @@ const MyButton = () => {
       <View style={{ backgroundColor: "#ddd", padding: 10, margin: 10 }}>
         {/* Button의 style 적용 */}
         {/* margin은 버튼 밖을 클릭해도 클릭이 되도록 한다 + 공간 차지 */}
-        <Text style={{ fontsize: 20, color: "maroon" }}>My Button</Text>
+        <Text style={{ fontsize: 20, color: "maroon" }}>
+          {children || title}
+        </Text>
       </View>
     </TouchableOpacity>
   );
+};
+
+// 아래와 같이 props의 기본값을 설정할 수 있다(2번 방법)
+MyButton.defaultProps = {
+  title: "default title",
+  onPress: () => alert("default"),
+};
+
+// 아래와 같이 props의 type을 지정할 수 있다
+MyButton.propTypes = {
+  title: PropTypes.string,
+  onPress: PropTypes.func,
+  // 아래와 같이 필수로 받아와야 하는 props를 지정해줄 수 있다
+  // subtitle: PropTypes.string.isRequired,
 };
 
 export default MyButton;
