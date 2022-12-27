5562e21e2354be6b0681604d47ba53ecbcd93f81
Hyogyeong8 <khg4773@gmail.com>
2022-12-27 14:49:00 +0900

    Add: styles.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-// import { styles, orangeText } from "./style";
+import { Text, View } from "react-native";
+import { styles, orangeText } from "./style";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, Diana</Text>
-      <Text style={styles.errorText}>Error message</Text>
+      <Text style={[styles.text, orangeText]}>Hello, Diana</Text>
+      <Text style={[styles.text, styles.errorText]}>Error message</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    backgroundColor: "#e1f6fc",
-    color: "#162529",
-    fontsize: 20,
-  },
-  errorText: {
-    color: "red",
-  },
-});
---
+++ src/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    backgroundColor: "#e1f6fc",
+    color: "navy",
+    fontsize: 20,
+  },
+  errorText: {
+    color: "red",
+  },
+});
+
+export const orangeText = {
+  color: "orange",
+};
+
+export default styles;
