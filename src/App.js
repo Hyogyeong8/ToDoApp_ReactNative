6972bc689c5ca46f601a337448486933b97d6894
Hyogyeong8 <khg4773@gmail.com>
2023-01-04 14:14:37 +0900

    Add: change loading page & install loadding app in expo

--- src/App.js
+++ src/App.js
@@ -8,119 +8,119 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import AppLoading from "expo-app-loading";
 
 const Container = styled.SafeAreaView`
-  flex: 1;
-  background-color: ${({ theme }) => theme.background};
-  align-items: center;
-  justify-content: flex-start;
+    flex: 1;
+    background-color: ${({ theme }) => theme.background};
+    align-items: center;
+    justify-content: flex-start;
 `;
 
 const Title = styled.Text`
-  font-size: 40px;
-  font-weight: 600;
-  color: ${({ theme }) => theme.main};
-  width: 100%;
-  align-items: flex-end;
-  padding: 0px 20px;
+    font-size: 40px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.main};
+    width: 100%;
+    align-items: flex-end;
+    padding: 0px 20px;
 `;
 
 // const List = styled.ScrollView.attrs(()=>({contentContainerStyle: {{allignItems: "center"}}}))`
 const List = styled.ScrollView`
-  flex: 1;
-  width: ${({ width }) => width - 40}px;
+    flex: 1;
+    width: ${({ width }) => width - 40}px;
 `;
 
 export default function App() {
-  const width = Dimensions.get("window").width;
+    const width = Dimensions.get("window").width;
 
-  const [tasks, setTasks] = useState({});
+    const [tasks, setTasks] = useState({});
 
-  const storeData = async (tasks) => {
-    try {
-      await AsyncStorage.setItem("tasks", JSON.stringify(tasks));
-      setTasks(tasks);
-    } catch (e) {
-      //
-    }
-  };
-
-  const getData = async () => {
-    const loadedData = await AsyncStorage.getItem("tasks");
-    setTasks(JSON.parse(loadedData || "{}"));
-  };
-
-  const [newTask, setNewTask] = useState("");
-
-  // const tempData = {
-  //   1: { id: "1", text: "React Native", completed: false },
-  //   2: { id: "2", text: "Expo ", completed: true },
-  //   3: { id: "3", text: "JavaScript", completed: false },
-  // };
-
-  const addTask = () => {
-    if (newTask.length < 1) return;
-    const ID = Date.now().toString();
-    const newTaskObject = {
-      [ID]: { id: ID, text: newTask, completed: false },
+    const storeData = async (tasks) => {
+        try {
+            await AsyncStorage.setItem("tasks", JSON.stringify(tasks));
+            setTasks(tasks);
+        } catch (e) {
+            //
+        }
     };
-    setNewTask("");
-    storeData({ ...tasks, ...newTaskObject });
-  };
 
-  const deleteTask = (id) => {
-    const currentTasks = Object.assign({}, tasks);
-    delete currentTasks[id];
-    storeData(currentTasks);
-  };
+    const getData = async () => {
+        const loadedData = await AsyncStorage.getItem("tasks");
+        setTasks(JSON.parse(loadedData || "{}"));
+    };
 
-  const toggleTask = (id) => {
-    const currentTasks = Object.assign({}, tasks);
-    currentTasks[id]["completed"] = !currentTasks[id]["completed"];
-    storeData(currentTasks);
-  };
+    const [newTask, setNewTask] = useState("");
 
-  const updaateTask = (item) => {
-    const currentTasks = Object.assign({}, tasks);
-    currentTasks[item.id] = item;
-    storeData(currentTasks);
-  };
+    // const tempData = {
+    //   1: { id: "1", text: "React Native", completed: false },
+    //   2: { id: "2", text: "Expo ", completed: true },
+    //   3: { id: "3", text: "JavaScript", completed: false },
+    // };
 
-  const [isReady, setIsReady] = useState(false);
+    const addTask = () => {
+        if (newTask.length < 1) return;
+        const ID = Date.now().toString();
+        const newTaskObject = {
+            [ID]: { id: ID, text: newTask, completed: false },
+        };
+        setNewTask("");
+        storeData({ ...tasks, ...newTaskObject });
+    };
 
-  return isReady ? (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor={theme.background}
+    const deleteTask = (id) => {
+        const currentTasks = Object.assign({}, tasks);
+        delete currentTasks[id];
+        storeData(currentTasks);
+    };
+
+    const toggleTask = (id) => {
+        const currentTasks = Object.assign({}, tasks);
+        currentTasks[id]["completed"] = !currentTasks[id]["completed"];
+        storeData(currentTasks);
+    };
+
+    const updaateTask = (item) => {
+        const currentTasks = Object.assign({}, tasks);
+        currentTasks[item.id] = item;
+        storeData(currentTasks);
+    };
+
+    const [isReady, setIsReady] = useState(false);
+
+    return isReady ? (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor={theme.background}
+                />
+                <Title>ToDO List</Title>
+                <Input
+                    placeholder="+ Add a Task"
+                    value={newTask}
+                    onChangeText={(text) => setNewTask(text)}
+                    onSubmitEditing={addTask}
+                    onBlur={() => setNewTask("")}
+                />
+                <List width={width}>
+                    {Object.values(tasks)
+                        .reverse()
+                        .map((item) => (
+                            <Task
+                                key={item.id}
+                                item={item}
+                                deleteTask={deleteTask}
+                                toggleTask={toggleTask}
+                                updateTask={updaateTask}
+                            />
+                        ))}
+                </List>
+            </Container>
+        </ThemeProvider>
+    ) : (
+        <AppLoading
+            startAsync={getData}
+            onFinish={() => setIsReady(true)}
+            onError={() => {}}
         />
-        <Title>ToDO List</Title>
-        <Input
-          placeholder="+ Add a Task"
-          value={newTask}
-          onChangeText={(text) => setNewTask(text)}
-          onSubmitEditing={addTask}
-          onBlur={() => setNewTask("")}
-        />
-        <List width={width}>
-          {Object.values(tasks)
-            .reverse()
-            .map((item) => (
-              <Task
-                key={item.id}
-                item={item}
-                deleteTask={deleteTask}
-                toggleTask={toggleTask}
-                updateTask={updaateTask}
-              />
-            ))}
-        </List>
-      </Container>
-    </ThemeProvider>
-  ) : (
-    <AppLoading
-      startAsync={getData}
-      onFinish={() => setIsReady(true)}
-      onError={() => {}}
-    />
-  );
+    );
 }
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 export const theme = {
-  background: "#111111",
-  itemBackground: "#333333",
-  main: "#668bdd",
-  text: "lavenderblush",
-  done: "#666666",
+    background: "#111111",
+    itemBackground: "#333333",
+    main: "#668bdd",
+    text: "#fff0f5",
+    done: "#666666",
 };
