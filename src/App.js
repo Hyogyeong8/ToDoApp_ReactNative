727c552a676b06e99fa3e5c18b0bc897d15f2cac
Hyogyeong8 <khg4773@gmail.com>
2023-01-04 19:39:27 +0900

    Refactor: fix typo

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ export default function App() {
                     barStyle="light-content"
                     backgroundColor={theme.background}
                 />
-                <Title>ToDO List</Title>
+                <Title>ToDo List</Title>
                 <Input
                     placeholder="+ Add a Task"
                     value={newTask}
