1734fb9d24faba7228830c7ff24119996db20b23
Hyogyeong8 <khg4773@gmail.com>
2023-01-06 14:15:10 +0900

    Add: components/User.js for Input component

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 import { UserConsumer } from "../context/User";
 
@@ -7,11 +7,34 @@ const StyledText = styled.Text`
   font-weight: 600;
 `;
 
+const StyledInput = styled.TextInput`
+  border: 1px solid #000000;
+  font-size: 24px;
+  padding: 10px;
+  width: 100%;
+`;
+
 const User = () => {
+  const [text, setText] = useState("");
+
   return (
-    <UserConsumer>
-      {({ name }) => <StyledText>Name: {name}</StyledText>}
-    </UserConsumer>
+    <>
+      <UserConsumer>
+        {({ name }) => <StyledText>Name: {name}</StyledText>}
+      </UserConsumer>
+      <UserConsumer>
+        {({ setName }) => (
+          <StyledInput
+            value={text}
+            onChangeText={setText}
+            onSubmitEditing={() => {
+              setName(text);
+              setText("");
+            }}
+          />
+        )}
+      </UserConsumer>
+    </>
   );
 };
 
