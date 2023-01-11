3fb9e107a5925648ce950290b6b08843e8fed0cf
Hyogyeong8 <khg4773@gmail.com>
2023-01-11 13:39:38 +0900

    Fix: input.js typo

--- src/component/input.js
+++ src/component/input.js
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "styled-components/native";
-import { Dimensions, useWindowDimensions } from "react-native";
-import PropTypes from "prop-types";
+import React from 'react';
+import styled from 'styled-components/native';
+import { Dimensions, useWindowDimensions } from 'react-native';
+import PropTypes from 'prop-types';
 
 const StyledInput = styled.TextInput.attrs(({ theme }) => ({
   placeholderTextColor: theme.main,
@@ -30,10 +30,10 @@ const Input = ({
       width={width}
       placeholder={placeholder}
       maxLength={50}
-      autoCapital="none" // 자동으로 대문자로 바뀌는 기능 끄기
+      autoCapitalize='none' // 자동으로 대문자로 바뀌는 기능 끄기
       autoCorrect={false} // 자동 오타 수정 기능 끄기
-      returnKeyType="done" // iOS에서 return키를 done으로 나오게 설정
-      keyboardAppearance="dark" // iOS에서만 키보드 색상을 어둡게 변경
+      returnKeyType='done' // iOS에서 return키를 done으로 나오게 설정
+      keyboardAppearance='dark' // iOS에서만 키보드 색상을 어둡게 변경
       value={value}
       onChangeText={onChangeText}
       onSubmitEditing={onSubmitEditing}
